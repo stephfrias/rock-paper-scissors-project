be7698bf50f10e4198c9e13e863b7ebaf58f9e14
Stephanie <steph.frias01@me.com>
2024-04-08 09:08:04 -0400

    created randomChoice & winner function

--- main.js
+++ main.js
@@ -26,3 +26,28 @@ Function winner(player1, player2)
     Return "Player 2 Wins!"
 */
 
+const prompt = require('prompt-sync')({sigint:true});
+
+function randomChoice() {
+    const choice = Math.floor(Math.random() * 3); 
+    if (choice === 0) {
+        return "rock";
+    } else if (choice === 1) {
+        return "paper";
+    } else {
+        return "scissors";
+    }
+}
+
+// Function to determine the winner of the game
+function winner(player1, player2) {
+    if (player1 === player2) {
+        return "It's a tie";
+    } else if ((player1 === "rock" && player2 === "scissors") ||
+               (player1 === "paper" && player2 === "rock") ||
+               (player1 === "scissors" && player2 === "paper")) {
+        return "Player 1 Wins!";
+    } else {
+        return "Player 2 Wins!";
+    }
+}
